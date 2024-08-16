e6ad9bd9cf3eb0ef384320fd9a5e7b7af4c0b95a
Haroon Jamal <Haroon.j@pacsquare.com>
2024-08-16 08:48:30 +0500

    Changes for Road MAp

--- cypress/e2e/utils/dataGenerator.js
+++ cypress/e2e/utils/dataGenerator.js
@@ -8,12 +8,12 @@ const getRandomDateBetween = (startDate, endDate) => {
 };
 
 // Function to format the date as MM/DD/YYYY
-const formatDate = (date) => {
-    const day = String(date.getDate()).padStart(2, '0');
-    const month = String(date.getMonth() + 1).padStart(2, '0'); // Months are 0-based
-    const year = date.getFullYear();
-    return `${month}/${day}/${year}`;
-};
+// const formatDate = (date) => {
+//     const day = String(date.getDate()).padStart(2, '0');
+//     const month = String(date.getMonth() + 1).padStart(2, '0'); // Months are 0-based
+//     const year = date.getFullYear();
+//     return `${month}/${day}/${year}`;
+// };
 
 
 export const generatePersonalInfoData=()=>{
